5fa84a14744f0c51b5a9aba151304c32f22e2f86
Mahigavale <maheshgavale07@gmail.com>
2024-07-29 16:32:26 +0530

    keywords,prompt

---
+++ Javascript/Keyword.js
@@ -0,0 +1,18 @@
+{
+  name1 = "mahesh";
+  //name2 = "mansi";
+
+  var name1;
+
+  let name2 = "mansi";
+  name2 = "mansi2";
+  console.log(name2);
+
+  const name3 = "dada";
+  console.log(name3);
+}
+name1 = "changed";
+
+console.log(name1); //var => global
+//console.log(name2);// block level
+//console.log(name3); block level
