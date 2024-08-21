64f4588d74100099ca8d32b862ff28681fb4c2a5
Mahigavale <maheshgavale07@gmail.com>
2024-08-21 12:54:59 +0530

    The array

---
+++ Javascript/ArrayJs/Array1.js
@@ -0,0 +1,18 @@
+const profiles = [
+  { name: "mahesh", age: 23 },
+  { name: "mansi", age: 24 },
+  { name: "Kd", age: 34 },
+];
+
+console.log(profiles);
+console.log("hehe");
+
+console.log(profiles[1].name);
+profiles[1] = 23;
+console.log(profiles[1]);
+console.log(profiles);
+
+//array indices start from 0.
+//in java only a single type of the data
+//in js , various types of data can be stored.
+const Songs = [12, "mahesh", 45.67, "c", 35.6];
